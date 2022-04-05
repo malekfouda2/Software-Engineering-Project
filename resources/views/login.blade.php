e45f9e6371a0bf3fd1fc09e4912c0518be41a5c3
omar1916647 <86333366+omar1916647@users.noreply.github.com>
2022-04-05 05:23:50 +0200

    Add files via upload

---
+++ resources/views/login.blade.php
@@ -0,0 +1,142 @@
+@if(session('addmessage'))
+    <script>alert('{{session('addmessage')}}');</script>
+@endif
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Gym System">
+    <meta name="keywords" content="Activitar, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" type="image/png" href="img/icons/chose-icon-4.png"/>
+    <title>Gym System</title>
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,600,700,800,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:300,400,500,600,700&display=swap" rel="stylesheet">
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="css/nice-select.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+
+<body style="background-color: #151515;">
+<!-- Page Preloder -->
+<div id="preloder">
+    <div class="loader"></div>
+</div>
+
+<!-- Header Section Begin -->
+@include('navbar')
+<!-- Header End -->
+
+<section class="breadcrumb-section set-bg spad" data-setbg="img/about-bread.jpg">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="breadcrumb-text">
+                    <h2>Login</h2>
+                    <div class="breadcrumb-controls">
+                        <a href="/"><i class="fa fa-home"></i> Home</a>
+                        <span>Login</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Contact Section Begin -->
+<section class="contact-section spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 offset-lg-1">
+                <div class="contact-form">
+                    <h4>Fill the fields!</h4>
+                    @if($errors->first('email') != "")
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            {{$errors->first('email')}}
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                    @endif
+                    @if($errors->first('password') != "")
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            {{$errors->first('password')}}
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                    @endif
+                    @if(session('loginmessage'))
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            Incorrect username or password.
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                    @endif
+                    <form action="" method="post">
+                        @csrf
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <label for="email" style="color: white;">
+                                    Email
+                                </label>
+                                <input type="text" placeholder="Email" name="email" value="{{old('email')}}">
+                            </div>
+                            <div class="col-lg-6">
+                                <label for="password" style="color: white;">
+                                    Password
+                                </label>
+                                <input type="password" placeholder="Password" name="password">
+                            </div>
+                            <div class="col-lg-12" style="text-align: center;">
+                                <button type="submit" class="c-btn">Login</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Contact Section End -->
+
+
+<!-- Footer Section Begin -->
+<footer class="footer-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+
+            </div>
+
+
+
+        </div>
+    </div>
+</footer>
+<!-- Footer Section End -->
+
+<!-- Js Plugins -->
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/mixitup.min.js"></script>
+<script src="js/jquery.nice-select.min.js"></script>
+<script src="js/jquery.slicknav.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/masonry.pkgd.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+
+</html>
